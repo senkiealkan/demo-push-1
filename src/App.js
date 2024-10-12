fc681ff7e03d12eb9ef3d845f0ee74fdc82e4b14
senkiealkan <22520008@gm.uit.edu.vn>
2024-10-12 10:28:29 +0700

    first commit~

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const orders  = [100, 200, 300]
 
 function App() {
+
+  
+  const [counter, setCounter] = useState(  () => {
+    const total = orders.reduce((total, cur) => total + cur)
+    console.log(total)
+    return total
+  });
+  const handleIncrease = () =>{
+    setCounter(prevState => prevState + 1);
+
+
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   );
 }
